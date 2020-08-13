33c0a6319236579349292d0b5d62b8291e2378f7
emeryberger <emery.berger@gmail.com>
2020-08-13 13:48:53 -0400

    Initial.

---
+++ libhoard.rb
@@ -0,0 +1,26 @@
+class Libhoard < Formula
+  desc "Fast malloc replacement"
+  homepage "https://github.com/emeryberger/Hoard"
+
+  head do
+    url "https://github.com/emeryberger/Hoard.git", :branch => "master"
+  end
+
+  def install
+#    inreplace "heaplayers-make.mk", /git clone https:\/\/github.com\/emeryberger\/Heap-Layers/, "/usr/bin/true"
+#    system "git", "clone", "https://github.com/emeryberger/Heap-Layers"
+    system "cd src; make"
+
+    lib.install "src/libhoard.dylib"
+    
+    (buildpath/"runner_script").write(runner_script)
+    bin.install "runner_script" => "hoard"
+  end
+  
+  def runner_script; <<-EOS
+#!/usr/bin/env sh
+DYLD_INSERT_LIBRARIES=#{lib}/libhoard.dylib "$@"
+  EOS
+  end
+
+end
